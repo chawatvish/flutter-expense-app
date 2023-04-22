dc7c5d4e026f7edb5951dee7f2bce9bd7487d79b
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2023-04-22 16:48:55 +0700

    init project

---
+++ lib/expenses.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ExpensesScreen extends StatelessWidget {
+  const ExpensesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expense_app/expenses.dart';
+
+void main() {
+  runApp(const ExpensesScreen());
+}
